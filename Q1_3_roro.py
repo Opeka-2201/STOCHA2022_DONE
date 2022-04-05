cc2bfaa6f12408b2cfa9019e54d0eb08ce8c1387
Romain Lambermont <lambermontr@gmail.com>
2022-04-05 13:15:35 +0200

    roro q3 ajouté, il manque convCheck et y_gen 3

--- Q1_3_roro.py
+++ Q1_3_roro.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sympy import false, true
 import matplotlib as plt
-
+from scipy.stats import binom
 K = 10
 p = 0.3
 r = 0.1
@@ -21,20 +21,19 @@ def q_distrib(y, x):
 
 
 def bin(k):
-    return np.random.binomial(K, p, k)
+    x = binom(K, p).pmf(k)
+    return x
 
 
 def alphaCalc(x0, y):
     alpha = (bin(y)/bin(x0))*(q_distrib(x0, y)/q_distrib(y, x0))
+    print(alpha)
     return min(alpha, 1)
 
 
 def y_gen(x0):
-    return np.random.random()  # TODO
-
-
-def convCheck():
-    return false  # TODO
+    x = np.random.randint(0, 11)
+    return q_distrib(x,x-1)
 
 
 def mhNextStep(x0):
@@ -53,13 +52,16 @@ def mhNextStep(x0):
 def mhAll(x_start):
     result = list()
     result.append((x_start, true))
-    conv = true
-    while not conv:
+    x0 = x_start
+    t = 0
+    tolérance  = 10**2
+    while t < tolérance:
         result.append(mhNextStep(x0))
         x0 = result[-1][0]
-        conv = convCheck()
+        t += 1
     return result
 
+x_start = 5
 
-x_start = 0
-result = mhAll(x_start)
+result = y_gen(x_start)
+print(result)
