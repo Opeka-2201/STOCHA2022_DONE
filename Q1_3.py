8e980f803d78aa2a3176fe5737b448d14e9f43b3
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2022-04-05 09:35:15 +0200

    go roro

--- Q1_3.py
+++ Q1_3.py
@@ -13,19 +13,19 @@ def q_distrib(Y, sachX, r, K):
     else:
         return 0
 
-def metropolis_hastings(r):
-    xt_1 = 0
-    t = 1
-    K = 10
-    while convergence:
-        yt = q_distrib(np.random.binomial(K,0.3),xt_1,r,K)
-        alpha = np.min(1, binomial(yt) * q_distrib(xt_1, yt, r, K) / (binomial(xt_1) * q_distrib(yt,xt_1, r, K)))
-
-        
-    pass
+def metropolis_hastings_etape(xt_1,r):
+    K=10
+    yt = q_distrib(np.random.binomial(K,0.3),xt_1,r,K)
+    alpha = np.min(1, binomial(yt) * q_distrib(xt_1, yt, r, K) / (binomial(xt_1) * q_distrib(yt,xt_1, r, K)))
+    u = np.random.uniform(0,1)
+    if(u < alpha):
+        x1 = yt
+    else:
+        x1 = xt_1
+    return x1
+x = 10
+for i in range(100):
+    x = metropolis_hastings_etape(x,0.1)
 
 def binomial(k):
     return np.random.binomial(10,0.3,k)
-
-def convergence(yt,yt_1):
-    pass
